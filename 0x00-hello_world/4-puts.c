b29dc1815eb22adbbbcc67faf6deed54e0b558db
rosemarygwen <onyetinted.ot@gmail.com>
2023-03-16 16:01:12 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ *main - A program to print line with put function
+ *Return: 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
