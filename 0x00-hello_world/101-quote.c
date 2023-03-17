a159e391d7a61af40d6b8d1f0418d3546b77f4ad
rosemarygwen <onyetinted.ot@gmail.com>
2023-03-16 18:55:49 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line of the standard error
+ * write - give standard output
  * Return 1 (success)
  */
 int main(void)
 {
-  write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-  return (1);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
-
-  
-    
