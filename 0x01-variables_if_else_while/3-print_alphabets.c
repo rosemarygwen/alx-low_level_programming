3ee0859f458485b158c9bcc5d11cede01686089a
rosemarygwen <onyetinted.ot@gmail.com>
2023-03-17 19:10:06 -0700

    task 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower and upper case
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
