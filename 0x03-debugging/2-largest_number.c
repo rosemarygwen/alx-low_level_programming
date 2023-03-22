3e0edb05c57e97cf12e4c3197e6656613672e220
rosemarygwen <onyetinted.ot@gmail.com>
2023-03-22 07:08:30 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
@@ -22,7 +22,7 @@ int largest_number(int a, int b, int c)
 	}
 	else if (c > b)
 	{
-		largest =  c;
+		largest = c;
 	}
 	else
 	{
