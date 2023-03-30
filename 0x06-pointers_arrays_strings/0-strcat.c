bcb172228e7516a8d50afdb62fb61709cf87e0bf
rosemarygwen <onyetinted.ot@gmail.com>
2023-03-30 16:20:25 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,11 @@ char *_strcat(char *dest, char *src)
 	int srclen = 0;
 	int i;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
-	for (i = 0 ; src[i] != '\0' ; i++)
+	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
-	for (i = 0 ; i <= srclen ; i++)
+	for (i = 0; i <= srclen; i++)
 		dest[destlen + i] = src[i];
 	return (dest);
 }
