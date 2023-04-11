52ffd99b020fcb129db577a88653a19930a0fe80
rosemarygwen <onyetinted.ot@gmail.com>
2023-04-10 18:11:27 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -9,6 +10,6 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv);
+	printf("%s\n", *argv);
 	return (0);
 }
