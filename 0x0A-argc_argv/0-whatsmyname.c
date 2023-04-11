ebbb18b9268e819bb3030265cc77c6595db0791b
rosemarygwen <onyetinted.ot@gmail.com>
2023-04-10 18:40:50 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - entry point
@@ -8,9 +7,9 @@
  * Return: (0)
  */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
