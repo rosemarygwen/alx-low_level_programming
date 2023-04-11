34047003e5c0e0fbe9bb17a4b6030683d0d50455
rosemarygwen <onyetinted.ot@gmail.com>
2023-04-10 18:02:15 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: parameter that counts the number of argument
+ * @argv: parameter that points to the array of arguments
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
