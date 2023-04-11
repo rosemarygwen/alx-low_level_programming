fb468601ec9fcaf83a7bc0515da6ad6e1e8f511a
rosemarygwen <onyetinted.ot@gmail.com>
2023-04-10 18:50:08 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: parameter that counts the number of argument
+ * @argv: parameter that points to the array of arguments
+ * Return: (0)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
