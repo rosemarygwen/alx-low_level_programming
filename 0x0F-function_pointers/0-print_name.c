094f4d5ca0f242e25ad85407a0565abf083b4cd9
rosemarygwen <onyetinted.ot@gmail.com>
2023-04-19 20:35:26 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - the function prints a name.
+ * @name: string to add
+ * @f: pointer to function that receives the string.
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
