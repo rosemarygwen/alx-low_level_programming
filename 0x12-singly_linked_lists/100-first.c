5abfc6ac36353ed38608f9a5a014697538acbe43
rosemarygwen <onyetinted.ot@gmail.com>
2023-04-27 21:54:09 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * premain - execute code before the main
+ * 
+ * return - nothing
+ */
+void __attribute__ ((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
