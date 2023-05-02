05db0197b0bef3549959e61ae55d07d390a5a510
rosemarygwen <onyetinted.ot@gmail.com>
2023-05-02 13:14:14 -0700

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,5 +10,21 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 void print_all(const char * const format, ...);
+void char_print(va_list c);
+void int_print(va_list i);
+void float_print(va_list f);
+void string_print(va_list s);
+
+/**
+ * struct format_specifier - format specifier structure
+ * @fmt_sign: param
+ * @fmt_func_ptr: format param
+ */
+
+typedef struct format_specifier
+{
+	char fmt_sign;
+	void (*fmt_func_ptr)(va_list);
+} fmt;
 
 #endif
