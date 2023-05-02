05c9749b3e8028aabf26790e2f4b853d9854c2e3
rosemarygwen <onyetinted.ot@gmail.com>
2023-05-02 13:41:07 -0700

    added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,5 @@ int _sqrt_recursion(int n);
 int is_sqrt(int num, int root);
 int is_prime_number(int n);
 int is_prime(int num, int root);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
